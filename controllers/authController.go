68951c40b8a3fe2cc92089dab01d492cab103cf7
Javier Gomez <javiergomezve@gmail.com>
2022-09-30 22:14:14 -0500

    [ADD] Register user

--- controllers/authController.go
+++ controllers/authController.go
@@ -2,14 +2,35 @@ package controllers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/javiergomezve/backend-admin/database"
 	"github.com/javiergomezve/backend-admin/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func RegisterUser(c *fiber.Ctx) error {
-	user := models.User{
-		FirstName: "Javier",
-		LastName:  "Gomez",
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
 	}
+
+	if data["password"] != data["password_confirm"] {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "passwords do not match",
+		})
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte(data["password"]), 14)
+
+	user := models.User{
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+		Password:  password,
+	}
+
+	database.DB.Create(&user)
 
 	return c.JSON(user)
 }
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/valyala/fasthttp v1.40.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	gorm.io/driver/mysql v1.3.6 // indirect
 	gorm.io/gorm v1.23.10 // indirect
