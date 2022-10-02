a7823121a50f62d7e358bba071881125d8a4ed39
Javier Gomez <javiergomezve@gmail.com>
2022-10-02 11:25:52 -0500

    [ADD] Rol CRUD

--- routes/route.go
+++ routes/route.go
@@ -18,4 +18,11 @@ func Setup(app *fiber.App) {
 	app.Get("/api/users/:id", controllers.GetUser)
 	app.Put("/api/users/:id", controllers.UpdateUser)
 	app.Delete("/api/users/:id", controllers.DeleteUser)
+	app.Get("/api/user", controllers.User)
+
+	app.Get("/api/roles", controllers.AllRoles)
+	app.Post("/api/roles", controllers.CreateRole)
+	app.Get("/api/roles/:id", controllers.GetRole)
+	app.Put("/api/roles/:id", controllers.UpdateRole)
+	app.Delete("/api/roles/:id", controllers.DeleteRole)
 }
