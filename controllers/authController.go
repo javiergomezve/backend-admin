05057cbe4efd59cc5ceb0f0f2d540ae4e826b5c1
Javier Gomez <javiergomezve@gmail.com>
2022-10-02 21:31:52 -0500

    [ADD] Update user profile information

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/javiergomezve/backend-admin/database"
 	"github.com/javiergomezve/backend-admin/models"
@@ -75,6 +77,51 @@ func User(c *fiber.Ctx) error {
 
 	var user models.User
 	database.DB.Where("id = ?", id).First(&user)
+
+	return c.JSON(user)
+}
+
+func UpdateInfo(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	id := util.ParseJwt(c)
+	fmt.Println(id)
+
+	user := models.User{
+		Id:        uint(id),
+		FirstName: data["first_name"],
+		LastName:  data["last_name"],
+		Email:     data["email"],
+	}
+
+	database.DB.Debug().Model(&user).Updates(user)
+
+	return c.JSON(user)
+}
+
+func UpdatePassword(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	if data["password"] != data["password_confirm"] {
+		c.SendStatus(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"message": "Passwords do not match",
+		})
+	}
+
+	id := util.ParseJwt(c)
+
+	user := models.User{
+		Id: uint(id),
+	}
+	user.SetPassword(data["password"])
+	database.DB.Model(&user).Where("id", id).Updates(user)
 
 	return c.JSON(user)
 }
